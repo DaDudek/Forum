c72295cbb9b920a0f9617418464eb3ba13176cf9
Dawid Dudek <dawiddudekstg@gmail.com>
2020-09-03 12:41:20 +0200

    secured password in database

--- src/forum/service/UserService.java
+++ src/forum/service/UserService.java
@@ -4,6 +4,10 @@ import forum.dao.DAOFactory;
 import forum.dao.UserDAO;
 import forum.model.User;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class UserService {
 
     public void createUser(String username, String email, String password){
@@ -31,8 +35,20 @@ public class UserService {
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(encryptPassword(password));
         user.setAccountActive(true); //in future change to send email with activate link
         return user;
+    }
+
+    private String encryptPassword(String password){
+        MessageDigest digest = null;
+        try{
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        digest.update(password.getBytes());
+        String md5Passwrod = new BigInteger(1, digest.digest()).toString(16);
+        return md5Passwrod;
     }
 }
